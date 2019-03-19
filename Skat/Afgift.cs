7b6e0c8813dc1f616ce3570c32ed8f142689892c
Dennis Kehlet Egholm <denn3205@edu.easj.dk>
2019-03-19 17:57:27 +0100

    Skat opgave

---
+++ Skat/Afgift.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Skat
+{
+    public class Afgift
+    {
+        
+        
+        /// <summary>
+        /// Metode til at udregne afgiften på biler som ikke er elbiler.
+        /// Metoden er delt op i to.
+        /// En med biler med en pris til om med 200000
+        /// og en med biler med en pris på over 200000
+        /// </summary>
+        /// <param name="pris"></param>
+        /// <returns></returns>
+        public int Bilafgift(int pris)
+        {
+            var bilAfgiftLav = Convert.ToInt32(pris * 0.85);
+            var bilAfgiftHøj1 = Convert.ToInt32(pris * 1.5);
+            var bilAfgiftHøj2 = bilAfgiftHøj1 - 130000;
+
+            if (pris <= 200000)
+            {
+                return bilAfgiftLav;
+            }
+            else
+            {
+                return bilAfgiftHøj2;
+            }
+        }
+
+        /// <summary>
+        /// Metode til at udregne afgiften på elbiler.
+        /// Metoden er delt op i to.
+        /// En med elbiler der har en pris til og med 200000
+        /// og en med elbiler der har en pris på mere end 200000
+        /// </summary>
+        /// <param name="pris"></param>
+        /// <returns></returns>
+        public int ElBilAfgift(int pris)
+        {
+            var bilAfgiftLav = Convert.ToInt32(pris * 0.85);
+            var bilAfgiftHøj1 = Convert.ToInt32(pris * 1.5);
+            var bilAfgiftHøj2 = bilAfgiftHøj1 - 130000;
+            var elBilAfgiftLav = Convert.ToInt32(bilAfgiftLav * 0.2);
+            var elBilAfgiftHøj = Convert.ToInt32(bilAfgiftHøj2 * 0.2);
+
+            if (pris <= 200000)
+            {
+                return elBilAfgiftLav;
+            }
+            else
+            {
+                return elBilAfgiftHøj;
+            }
+        }
+
+        
+    }
+}
---
+++ SkatTests/AfgiftTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Skat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skat.Tests
+{
+    [TestClass()]
+    public class AfgiftTests
+    {
+        [TestMethod()]
+        public void BilafgiftTestLav()
+        {
+            Afgift afgift1 = new Afgift();
+
+            var expected = 170000;
+            var actual = afgift1.Bilafgift(200000);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void BilafgiftTestHøj()
+        {
+            Afgift afgift2 = new Afgift();
+
+            var expected = 320000;
+            var actual = afgift2.Bilafgift(300000);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ElbilafgiftTestLav()
+        {
+            Afgift afgift3 = new Afgift();
+
+            var expected = 34000;
+            var actual = afgift3.ElBilAfgift(200000);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ElbilafgiftTestHøj()
+        {
+            Afgift afgift4 = new Afgift();
+
+            var expected = 64000;
+            var actual = afgift4.ElBilAfgift(300000);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+    
+    }
+}
